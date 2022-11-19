e8a846c83ca9922761db56373bc93fe4ea3f4021
vman <public@vaughnnugent.com>
2022-11-19 01:13:24 -0500

    License updates

--- VNLib.Plugins.Extensions.Loading/Events/IIntervalScheduleable.cs
+++ VNLib.Plugins.Extensions.Loading/Events/IIntervalScheduleable.cs
@@ -1,4 +1,28 @@
-﻿using System.Threading;
+﻿/*
+* Copyright (c) 2022 Vaughn Nugent
+* 
+* Library: VNLib
+* Package: VNLib.Plugins.Extensions.Loading
+* File: IIntervalScheduleable.cs 
+*
+* IIntervalScheduleable.cs is part of VNLib.Plugins.Extensions.Loading which is part of the larger 
+* VNLib collection of libraries and utilities.
+*
+* VNLib.Plugins.Extensions.Loading is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published
+* by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* VNLib.Plugins.Extensions.Loading is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with VNLib.Plugins.Extensions.Loading. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System.Threading;
 using System.Threading.Tasks;
 
 using VNLib.Utils.Logging;
--- VNLib.Plugins.Extensions.Validation/ValErrWebMessage.cs
+++ VNLib.Plugins.Extensions.Validation/ValErrWebMessage.cs
@@ -1,4 +1,28 @@
-﻿using System.Collections;
+﻿/*
+* Copyright (c) 2022 Vaughn Nugent
+* 
+* Library: VNLib
+* Package: VNLib.Plugins.Extensions.Validation
+* File: ValErrWebMessage.cs 
+*
+* ValErrWebMessage.cs is part of VNLib.Plugins.Extensions.Validation which is part of the larger 
+* VNLib collection of libraries and utilities.
+*
+* VNLib.Plugins.Extensions.Validation is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published
+* by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* VNLib.Plugins.Extensions.Validation is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with VNLib.Plugins.Extensions.Validation. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System.Collections;
 using System.Text.Json.Serialization;
 
 namespace VNLib.Plugins.Extensions.Validation
